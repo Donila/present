d03b04550829b3a591b619a0faa62d18a46c99e3
Donila <donila2008@gmail.com>
2013-07-31 20:51:30 +0300

    test

--- LevelUp/Attributes/AllowAnonymousAttribute.cs
+++ LevelUp/Attributes/AllowAnonymousAttribute.cs
@@ -5,5 +5,7 @@ namespace LevelUp.Attributes
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
     public sealed class AllowAnonymousAttribute : Attribute
     {
+
+
     }
 }
